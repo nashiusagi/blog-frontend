010e8f6fdd929dab64b1c327b6e0bc3236e79c47
nashiusagi <ritotech0226@gmail.com>
2024-01-12 23:58:23 +0900

    [add] PostCard

---
+++ components/PostCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { Post } from "types/api"
+
+interface Props {
+  post: Post
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div :class="$style.card">
+    <p :class="$style.title">{{ post.title }}</p>
+    <p>{{ post.body }}</p>
+  </div>
+</template>
+
+<style module>
+.card {
+  padding: 16px 12px;
+  border-radius: 4px;
+  box-shadow: 1px 1px 6px -1px #666;
+}
+
+.title {
+  font-size: 28px;
+  font-weight: bold;
+  font-style: normal;
+}
+</style>
