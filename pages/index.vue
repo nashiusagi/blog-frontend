57b87da26ff1c41b1f43ff894382e8469355eac3
nashiusagi <ritotech0226@gmail.com>
2024-01-12 22:43:05 +0900

    [add] 投稿一覧のプロトタイプを作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,33 @@
+<script setup lang="ts">
+import type { Post } from "types/api"
+
+const posts: Array<Post> = [
+  {
+    id: 1,
+    title: 'title1',
+    body: 'body1',
+  },
+  {
+    id: 2,
+    title: 'title2',
+    body: 'body2',
+  },
+]
+</script>
+
 <template>
-  index page
+  <div>
+    <div v-for="post in posts" :key="post.id">
+      <p :class="$style.title">{{ post.title }}</p>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
 </template>
+
+<style module>
+.title {
+  font-size: 28px;
+  font-weight: bold;
+  font-style: normal;
+}
+</style>
