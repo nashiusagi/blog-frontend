1b6d0f7f54ba73f7624b8270a47d9021588fd65f
nashiusagi <ritotech0226@gmail.com>
2024-01-15 21:27:04 +0900

    [add] eslint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@nuxtjs/eslint-config-typescript",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {
+    // セミコロンのルール
+    "semi": ["error", "never", { "beforeStatementContinuationChars": "never" }],
+    "semi-spacing": ["error", { "after": true, "before": false}],
+    "semi-style": ["error", "first"],
+    "no-extra-semi": "error",
+    "no-unexpected-multiline": "error",
+    "no-unreachable": "error",
+
+    // comma-dangleのルール
+    "comma-dangle" : ["error", {
+      "arrays": "never",
+      "objects": "always",
+      "imports": "never",
+      "imports": "never",
+      "exports": "never",
+      "functions": "never",
+    }],
+
+    // quoteのルール
+    "quotes": ["error", "double"],
+  },
+}
