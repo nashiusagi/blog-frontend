f3a9846e22cda1e8167b7f20915827094b325588
nashiusagi <ritotech0226@gmail.com>
2024-01-18 22:24:32 +0900

    [update] 正常なテストケースを追加

--- composables/__test__/useAllPosts.test.ts
+++ composables/__test__/useAllPosts.test.ts
@@ -1,11 +1,34 @@
 // @vitest-environment nuxt
-import { describe, test, expect } from "vitest"
-import { useAllPosts } from "../useAllPosts"
+import { describe, test, expect, vi } from "vitest"
+import { useAllPosts } from "@/composables/useAllPosts"
 
 describe("useAllPost test", () => {
-  test("", () => {
-    const pending = useAllPosts()
+  test("正常にデータを取得できる", async () => {
+    const mockData = [
+      {
+        id: 1,
+        title: "title1",
+        body: "body1",
+      },
+      {
+        id: 2,
+        title: "title2",
+        body: "body2",
+      },
+      {
+        id: 3,
+        title: "title3",
+        body: "body3",
+      }
+    ]
 
-    expect(pending).toBeTruthy()
+    const spy = vi.spyOn(global, "fetch").mockResolvedValue(new Response(JSON.stringify(mockData)))
+
+    const { data, pending, } = await useAllPosts()
+
+    expect(data.value.length).toBe(3)
+    expect(pending.value).toBeFalsy()
+
+    spy.mockRestore()
   })
 })
