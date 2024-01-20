e72d295bc53a988fe5d8252dbc9ca6cfe3e1bdf9
nashiusagi <ritotech0226@gmail.com>
2024-01-20 14:09:14 +0900

    [update] 既存ページのレイアウトを変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import type { Post } from '@/types/api'
 import MainLayout from '@/layouts/MainLayout.vue'
+import IndexTitleBlock from '@/components/index/IndexTitleBlock.vue'
+import CategoriesBlock from '@/components/common/CategoriesBlock.vue'
 
 const posts = ref<Post[]>([])
 
@@ -13,18 +15,24 @@ onMounted(async () => {
 <template>
   <MainLayout>
     <div :class="$style.index_body">
+      <IndexTitleBlock />
       <div :class="$style.posts_list">
         <div v-for="post in posts" :key="post.id">
           <PostCard :post="post" />
         </div>
       </div>
+      <CategoriesBlock />
     </div>
   </MainLayout>
 </template>
 
 <style module>
 .index_body {
-  padding: 16px 12px;
+  padding: 16px 24px;
+  gap: 24px;
+  display: flex;
+  flex-direction: column;
+  background-color: #f5f6f6;
 }
 
 .posts_list {
